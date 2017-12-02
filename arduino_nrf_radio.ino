93e3d93c2e1f07500cd48a9bff379b1aaa59e851
Antoine <ameisso@gmail.com>
2017-12-02 21:27:37 +0100

    Clean : spacing and indent

--- arduino_nrf_radio.ino
+++ arduino_nrf_radio.ino
@@ -5,9 +5,9 @@
 #include "radio_config.h"
 #include "nrf.h"
 
-#define NRF_SUCCESS 1
-static uint32_t packet;                    /**< Packet to transmit. */
 
+#define NRF_SUCCESS 1
+static uint32_t packet;
 
 void send_packet()
 {
@@ -26,8 +26,6 @@ void send_packet()
   {
     // wait
   }
-
-
   Serial.println("The packet was sent");
 
   NRF_RADIO->EVENTS_DISABLED = 0U;
@@ -42,17 +40,12 @@ void send_packet()
 
 void clock_initialization()
 {
-  /* Start 16 MHz crystal oscillator */
   NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
   NRF_CLOCK->TASKS_HFCLKSTART    = 1;
-
-  /* Wait for the external oscillator to start up */
   while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
   {
     // Do nothing.
   }
-
-  /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
   NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
   NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
   NRF_CLOCK->TASKS_LFCLKSTART    = 1;
@@ -66,24 +59,13 @@ void clock_initialization()
 
 
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
   uint32_t err_code = NRF_SUCCESS;
-
   clock_initialization();
-
-
-
-
-
-
-
   // Set radio configuration parameters
   radio_configure();
-
   // Set payload pointer
   NRF_RADIO->PACKETPTR = (uint32_t)&packet;
-
-
 }
 
 void loop() {
@@ -93,6 +75,5 @@ void loop() {
     Serial.println("The contents of the package was %u");//, (unsigned int)packet));
     packet = 0;
   }
-
   __WFE();
 }
